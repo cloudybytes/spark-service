b77125e6d51892865d3ae41f03c6af063a3da088
Avinash <vintas@users.noreply.github.com>
2021-09-28 23:25:27 +0530

    remove hardcoded dataset path

--- spark_run/models.py
+++ spark_run/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
@@ -7,7 +8,7 @@ scSpark = SparkSession \
     .appName("reading csv") \
     .getOrCreate()
 
-dataset_path ='F:\\4-1\\Cloud Computing\\CC_Ass1_dataset\\'
+dataset_path = settings.DATASET_PATH
 movie_file = dataset_path +'movies.csv'
 rating_file = dataset_path +'rating.csv'
 users_file = dataset_path +'users.csv'
@@ -16,11 +17,11 @@ movieData = scSpark.read.csv(movie_file, sep=",").toDF("movieid" , "title" , "re
 usersData = scSpark.read.csv(users_file, sep=",").toDF("userid" , "age" , "gender" , "occupation" , "zipcode").cache()
 ratingData = scSpark.read.csv(rating_file, sep=",").toDF("userid" , "movieid" , "rating" , "timestamp").cache()
 zipData = scSpark.read.csv(zip_file, sep=",").toDF("zipcode" , "zipcodetype" , "city" , "state").cache()
-print('Total Records = {}'.format(movieData.count()))
-print('Total Records = {}'.format(usersData.count()))
-print('Total Records = {}'.format(ratingData.count()))
-print('Total Records = {}'.format(zipData.count()))
-movieData.show()
-zipData.show()
+# print('Total Records = {}'.format(movieData.count()))
+# print('Total Records = {}'.format(usersData.count()))
+# print('Total Records = {}'.format(ratingData.count()))
+# print('Total Records = {}'.format(zipData.count()))
+# movieData.show()
+# zipData.show()
 
 
