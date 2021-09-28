dd4ffc860362119fcca743217b43578693320bfc
Gitansh Pujari <gitansh313@gmail.com>
2021-09-28 14:37:53 +0530

    load dataset WIP

--- spark_run/models.py
+++ spark_run/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
 
-# Create your models here.
+scSpark = SparkSession \
+    .builder \
+    .appName("reading csv") \
+    .getOrCreate()
+
+dataset_path ='F:\\4-1\\Cloud Computing\\CC_Ass1_dataset\\'
+movie_file = dataset_path +'movies.csv'
+rating_file = dataset_path +'rating.csv'
+users_file = dataset_path +'users.csv'
+zip_file = dataset_path +'zipcodes.csv'
+movieData = scSpark.read.csv(movie_file, sep=",").toDF("movieid" , "title" , "releasedate" , "unknown" , "Action" , "Adventure" , "Animation" , "Children" , "Comedy" , "Crime" , "Documentary" , "Drama" , "Fantasy" , "Film_Noir" , "Horror" , "Musical" , "Mystery" , "Romance" , "Sci_Fi" , "Thriller" , "War" , "Western" ).cache()
+usersData = scSpark.read.csv(users_file, sep=",").toDF("userid" , "age" , "gender" , "occupation" , "zipcode").cache()
+ratingData = scSpark.read.csv(rating_file, sep=",").toDF("userid" , "movieid" , "rating" , "timestamp").cache()
+zipData = scSpark.read.csv(zip_file, sep=",").toDF("zipcode" , "zipcodetype" , "city" , "state").cache()
+print('Total Records = {}'.format(movieData.count()))
+print('Total Records = {}'.format(usersData.count()))
+print('Total Records = {}'.format(ratingData.count()))
+print('Total Records = {}'.format(zipData.count()))
+movieData.show()
+zipData.show()
+
+
--- spark_run/views.py
+++ spark_run/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import movieData, ratingData, usersData, zipData
+from django.http import HttpResponse
 
-# Create your views here.
+def abcd(request):
+    print('Total Records = {}'.format(movieData.count()))
+    print('Total Records = {}'.format(usersData.count()))
+    print('Total Records = {}'.format(ratingData.count()))
+    print('Total Records = {}'.format(zipData.count()))
+    ratingData.show()
+    return HttpResponse("success")
+
