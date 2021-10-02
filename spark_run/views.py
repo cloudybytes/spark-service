164479c3907f1dd7c136fcd1eff76cc1d92c437f
Avinash <vintas@users.noreply.github.com>
2021-10-03 00:33:46 +0530

    remove url of static csv

--- spark_run/views.py
+++ spark_run/views.py
@@ -99,5 +99,5 @@ def p_query(request):
     working_dataframe.show()
     filename = uuid.uuid4().hex + '.csv'
     working_dataframe.toPandas().to_csv(os.path.join(settings.BASE_DIR, 'static', filename))
-    output_url = settings.URL + static(filename)
+    output_url = static(filename)
     return JsonResponse({'time': total_time_spark, 'output_url': output_url})
